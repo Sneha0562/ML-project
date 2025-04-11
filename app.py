855cfe1fd28302082c2ac1f1c3d4b33204ecd344
Your GitHub Username <your_email@example.com>
2025-04-11 15:42:48 +0530

    utils added

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+import sys
+import os
+
+# Add 'src' directory to Python path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 from mlProject.exception.exception_handler import AppException
 import sys
+from mlProject.logger.log import logging
 
 try:
     a=3/0
 except Exception as e:
-    raise AppException(e,sys) from e    
+    raise AppException(e,sys) from e  
+
+    loggig.info(e)
+    raise AppException(e,sys) from e 
